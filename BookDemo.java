e192ce9475dca34e4c528f5a51305690f7e7f0ca
Meaha <69106908+Meaha7@users.noreply.github.com>
2021-09-21 11:09:38 +0530

    Create BookDemo.java

---
+++ BookDemo.java
@@ -0,0 +1,48 @@
+class Book
+{                                             // Done By Meaha.J (123015056)
+	private String BKName;  
+        private int BKId;               // Private Data Members
+	private String BKAuthor;
+
+	public Book(String Bname ,int BId , String BAuthor)   //Parametrised Constructor
+	{
+		BKName=Bname;
+		BKId=BId;
+		BKAuthor=BAuthor;
+	}
+
+ 	public void BKUpdatedetails(String Name,int Id,String Author)  //Method to accept new value
+	{
+		BKName=Name;
+		BKId=Id;
+		BKAuthor=Author;
+	}
+
+ 	public void BKDisplay() // Method to display the Book Details
+	{
+		System.out.println("========================================");
+		System.out.println(" Book ID       : "+BKId);
+		System.out.println(" Book Name     : "+BKName);
+		System.out.println(" Book Author   : "+BKAuthor);
+		System.out.println("========================================");
+		
+	}
+}
+
+class BookDemo 
+{
+	public static void main(String[] args) // Main Function
+	{
+		System.out.println();	
+		System.out.println("=*=*=*=*=*=*=   Old Details    *=*=*=*=*");
+
+		Book ob=new Book("C Programming",100,"Dennis Ritchie");
+		ob.BKDisplay();
+
+		System.out.println();
+		System.out.println("=*=*=*=      Updated Details      =*=*=*");
+		ob.BKUpdatedetails("Java Programming",100,"James Gosling");
+		ob.BKDisplay();
+
+	}
+}
